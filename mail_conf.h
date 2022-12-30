75203e607d5fce35c25eb467b29034641645551d
Pierpaolo Garofalo <83761967+Pier1965@users.noreply.github.com>
2022-12-30 15:27:43 +0100

    Update mail_conf.h

--- mail_conf.h
+++ mail_conf.h
@@ -1,6 +1,6 @@
 /* CONFIGURAZIONE POSTA ELETTRONICA */
-#define RECIPIENT1 "pierundcarla@gmail.com"   // <------------ mail destinatario
-#define RECIPIENT2 "Pierpaolo Garofalo"       // <------------ nome destinatario
+#define RECIPIENT1 "RECIPIENT@gmail.com"   // <------------ mail destinatario
+#define RECIPIENT2 "DONALD DUCK"       // <------------ nome destinatario
 /** The smtp port e.g.
  * 25  or esp_mail_smtp_port_25
  * 465 or esp_mail_smtp_port_465
@@ -9,8 +9,8 @@
 #define SMTP_HOST "smtp.libero.it"            // <------------ servizio posta smtp mittente
 #define SMTP_PORT esp_mail_smtp_port_465      // <------------ porta smtp server
 /* The log in credentials */
-#define AUTHOR_EMAIL "pppc65@libero.it"       // <------------ mail mittente
-#define AUTHOR_PASSWORD "P@55libero"          // <------------ password mail mittente
+#define AUTHOR_EMAIL "RECIPIENT@gmail.com"       // <------------ mail mittente
+#define AUTHOR_PASSWORD "PASSWORD"          // <------------ password mail mittente
 #define ESP_NAME "ESP Mahler"
 #define OGGETTO "Test calorimetro di Mahler"
 #define MESSAGGIO "Rilievo profilo di temperatura prova con calorimetro di Mahler eseguito "
