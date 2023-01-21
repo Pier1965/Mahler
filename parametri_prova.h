bae94cbeac48d6edd4c009fe35af7bdc8b0b41c3
Pierpaolo Garofalo <83761967+Pier1965@users.noreply.github.com>
2023-01-21 18:59:37 +0100

    Delete parametri_prova.h

--- parametri_prova.h
+++
@@ -1,2 +0,0 @@
-#define N_SAMPLINGS 20            // <----------------- numero di misurazioni della temperatura da fare nel test
-#define DT_SAMPLINGS 500          // <----------------- intervallo di tempo tra le letture della temperatura
